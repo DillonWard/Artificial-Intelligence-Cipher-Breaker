db7502177da60be453a2d67019eec02d31602dcb
Dillon Ward <Dillonward13@gmail.com>
2018-03-21 17:03:34 +0000

    encoding and decoding string

---
+++ src/ie/gmit/sw/ai/ProxyRunner.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.ai;
+
+public class ProxyRunner {
+
+	
+	public static void main(String[] args) {
+		
+		Runner runner = new Runner();
+		
+		runner.Playfair();
+		
+	}
+}
---
+++ src/ie/gmit/sw/ai/Runner.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.ai;
+
+public class Runner {
+	
+	private static String keyword = "keyword";
+	private static String message = "this is the message";
+	Playfair pf;
+	
+	public void Playfair(){
+		pf = new Playfair(keyword, message);
+	}
+
+}
