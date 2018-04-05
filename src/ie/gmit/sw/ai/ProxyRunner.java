4a88d6a5c68aa59ec530b646a09fb91be788f8c1
Dillon <Dillonward13@gmail.com>
2018-04-05 20:42:22 +0100

    ui updated

--- src/ie/gmit/sw/ai/ProxyRunner.java
+++ src/ie/gmit/sw/ai/ProxyRunner.java
@@ -4,37 +4,47 @@ import java.util.Scanner;
 
 public class ProxyRunner {
 
-	
 	public static void main(String[] args) throws Throwable {
-		
-		Scanner input = new Scanner(System.in);	
+
+		Scanner input = new Scanner(System.in);
 		int choice = 0;
 		boolean running = true;
 		String fname = "";
-		
-		do{
+
+		do {
 			System.out.println("1: Encrypt a file \n2: Decrypt a file \n3: Exit");
 			choice = input.nextInt();
-			
-			
-		}while(running = true);
-		
-		
-		
-		
-		
-		Playfair playfair;
-		SimulatedAnnealing simulated;
-		Key key;
-		
-		String cipherKey = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
-		String fileName = "4grams.txt";
-		
-		String file = new FileParser().readFile(fileName);
-		
-		System.out.println(file);
-		
-		
-		
+
+			switch (choice) {
+			case 1:
+
+				break;
+
+			case 2:
+				break;
+
+			case 3:
+				running = false;
+				break;
+
+			default:
+				System.out.println("1: Encrypt a file \n2: Decrypt a file \n3: Exit");
+				choice = input.nextInt();
+				break;
+			}
+
+		} while (running);
+
+		// Playfair playfair;
+		// SimulatedAnnealing simulated;
+		// Key key;
+		//
+		// String cipherKey = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+		// String fileName = "4grams.txt";
+		//
+		// String file = new FileParser().readFile(fileName);
+		//
+		// System.out.println(file);
+
 	}
 }
